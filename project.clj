7c98a92a919de0c89b2c74523ff4260ed69dc5eb
Matt Gauger <matt.gauger@gmail.com>
2014-09-02 14:00:24 -0500

    Proof of concept config file

--- project.clj
+++ project.clj
@@ -3,8 +3,10 @@
   :url "https://github.com/mathias/phoenix-config"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :plugins [[lein-cljsbuild "1.0.3"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2322"]]
+  :plugins [[lein-cljsbuild "1.0.3"]
+            [lein-marginalia "0.7.1"]]
   :cljsbuild { :builds [{:source-paths ["src"]
                          :compiler {:output-to "target/phoenix.js"
                                     :optimizations :whitespace
--- src/phoenix_config/core.cljs
+++ src/phoenix_config/core.cljs
@@ -1,6 +1,32 @@
 (ns phoenix-config.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; Phoenix Globals
+
+(def Window js/Window)
+(def api js/api)
+
+;; Math helpers
+(defn round [num] (.round js/Math num))
+(defn half [num] (/ num 2))
+
+(defn pushLeft []
+  (let [win (.focusedWindow Window)
+        frame (.frame win)
+        screen (.. win screen frameWithoutDockOrMenu)]
+    (aset frame "x" (.-x screen))
+    (aset frame "y" (.-y screen))
+
+    (aset frame "width" (round (half (.-width screen))))
+    (aset frame "height" (.-height screen))
+
+    (.setFrame win frame)))
+
+(defn fullscreen []
+  (let [win (.focusedWindow Window)
+        screen (.. win screen frameWithoutDockOrMenu)]
+    (.setFrame win screen)))
+
+(def mash (js/Array. "ctrl" "alt" "cmd"))
+
+(.bind api "left" mash pushLeft)
+(.bind api "m" mash fullscreen)
