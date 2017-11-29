4f30267be3402a1aeabf4c7c9fa5eacb05447967
Matt Gauger <mathias@heroku.com>
2017-11-29 14:36:58 -0600

    Vertical splits working. Moving windows and bindings working

--- phoenix.js
+++ phoenix.js
@@ -1,4 +1,4 @@
-/* globals Phoenix Window App Key */
+/* globals Phoenix Modal Window App Key */
 
 /*
  * This is a self-documenting configuration for
@@ -24,81 +24,77 @@
 /* ## Config begins here */
 
 Phoenix.notify('Phoenix config loading')
+Phoenix.set({
+  openAtLogin: true,
+})
+
 
 /* Phoenix helper functions */
 
-var focused = Window.focused;
-var visibleWindows = Window.recent;
+var focused = Window.focused
+var visibleWindows = Window.recent
 
 /* Math helpers */
 
-var round = function(num) {
-  return Math.round(num);
+var round = function (num) {
+  return Math.round(num)
 }
 
 /* Development helpers */
 
-var debug = function(message) {
-  return Phoenix.alert(message, 5);
+var debug = function (message) {
+  return Phoenix.notify(message)
 }
 
-var log = function(message) {
-  return Phoenix.log(message);
+var log = function (message) {
+  return Phoenix.log(message)
 }
-
-/* Window focus operations */
-
-/* Application Launching: TODO */
 
 /* Grid functions */
 
-var calculateGrid = function(coords) {
-  var screenSize = Screen.main().visibleFrame();
-  return {
-    x: round((screenSize.width * requested.x) + screenSize.x)
-  };
+var calculateGrid = function (coords) {
+  var screenFrame = Screen.main().visibleFrame()
+  var newFrame = {
+    x: screenFrame.width * coords.x,
+    y: screenFrame.height * coords.y,
+    width: screenFrame.width * coords.width,
+    height: screenFrame.height * coords.height
+  }
+  debug(JSON.stringify(newFrame));
+  return newFrame;
 }
 
-var sizeToGrid = function(coords) {
-  var screen = Screen.main().flippedVisibleFrame();
-  var window = Window.focused();
-  var frame = {
-    x: screen.origin().x,
-    y: screen.origin().y,
-    width: screen.width()/ 2,
-    height: screen.height() / 2
-  };
-
+var sizeToGrid = function (coords) {
+  var screen = Screen.main().flippedVisibleFrame()
+  var window = Window.focused()
+  var frame = calculateGrid(coords);
 
   if (window) {
-    return window.setFrame({
-      x: round(screen.x + (screen.width * coords.x) - frame.width),
-      y: round(screen.y + (screen.height / coords.y) - frame.height),
-
-    });
+    return window.setFrame(frame);
   }
 }
 
-var snapAllToGrid = function() {
-  focused().map(function(win) { return win.snapToGrid(); })
-};
+var snapAllToGrid = function () {
+  focused().map(function (win) { return win.snapToGrid() })
+}
 
 /* Movement functions */
-var pushLeft = function() { return sizeToGrid({x: 0, y: 0, width: 0.5, height: 1}); };
-
+var pushLeft = function () { return sizeToGrid({x: 0, y: 0, width: 0.5, height: 1}) }
+var pushRight = function () { return sizeToGrid({x: 0.5, y: 0, width: 0.5, height: 1}) }
 
 /* Keybindings */
 
-var emptyMods = [];
-var mash = ['ctrl', 'alt', 'cmd'];
-var superK = ['ctrl', 'cmd'];
-var superMeta = ['alt', 'cmd'];
+var emptyMods = []
+var mash = ['ctrl', 'alt', 'cmd']
+var superK = ['ctrl', 'cmd']
+var superMeta = ['alt', 'cmd']
 
-var bind = function(letter, chord, fn) {
-  return Key.on(letter, chord, fn);
+var bind = function (letter, chord, fn) {
+  return Key.on(letter, chord, fn)
 }
 
-bind('left', mash, pushLeft);
+bind('left', mash, pushLeft)
 
 /* vim-style bindings for HHKB */
-bind('h', superK, pushLeft);
+bind('h', superK, pushLeft)
+bind('l', superK, pushRight)
